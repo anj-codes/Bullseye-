448dda05c080e15d3369a431a18448cddd148cf0
anj-codes <120025904+anj-codes@users.noreply.github.com>
2022-12-14 22:39:45 +0800

    Bullseye Basic View

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,17 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                Text("89")
+                HStack {
+                    Text("1")
+                    Slider(value: .constant(50), in: 1.0...100.0)
+                    Text("100")
+                }
+                Button("Hit me") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .previewLayout(.fixed(width:568, height:320))
     }
 }
