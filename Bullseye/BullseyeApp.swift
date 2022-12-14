7ec9a69cda817f95395a0d390aeb22c15db20e15
anj-codes <120025904+anj-codes@users.noreply.github.com>
2022-12-14 21:51:16 +0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Anj Licudine on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
